47032e7c28a7dddd7001ed29725508f46831b073
HangNT <HangNT169@fpt.edu.vn>
2024-05-05 21:17:47 +0700

    Initial commit

---
+++ src/main/java/com/poly/soff3021/Soff3021Application.java
@@ -0,0 +1,13 @@
+package com.poly.soff3021;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Soff3021Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Soff3021Application.class, args);
+    }
+
+}
